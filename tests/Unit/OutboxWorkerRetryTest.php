8e379acee1c8c4255916d39d79cbcb084104350e
Oleksandr Tishchenko <taa0662621456@gmail.com>
2026-03-26 20:23:23 -0500

    Normalize PHP copyright headers to single-line hash format

--- tests/Unit/OutboxWorkerRetryTest.php
+++ tests/Unit/OutboxWorkerRetryTest.php
@@ -1,7 +1,5 @@
 <?php
-
-// Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
-
+# Copyright (c) 2025 Oleksandr Tishchenko / Marketing America Corp
 declare(strict_types=1);
 
 namespace App\Tests\Unit;
